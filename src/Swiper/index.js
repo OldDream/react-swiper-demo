28063a4d41d1e8ce52804091d275a0d550aa88a7
OldDream <huangyn15@qq.com>
2021-06-21 02:02:11 +0800

    add：仿造成功

--- src/Swiper/index.js
+++ src/Swiper/index.js
@@ -20,8 +20,10 @@ const slidesArr = [];
 
 for (let index = 1; index <= 4; index++) {
   slidesArr.push(
-    <SwiperSlide>
-      <img src={imgs[`pg${index}`]} alt="" />
+    <SwiperSlide className="swiper-slide" key={index}>
+      <div>
+        <img src={imgs[`pg${index}`]} alt="" />
+      </div>
     </SwiperSlide>
   );
 }
@@ -29,11 +31,56 @@ for (let index = 1; index <= 4; index++) {
 const SwiperWrapper = () => {
   return (
     <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
+      resistanceRatio={0}
+      watchSlidesProgress={true}
+      className="swiper-wrapper"
+      slidesPerView={1}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
-      onSlidePrev={() => console.log('slide onSlidePrev')}
+      onInit={(swiper) => {
+        const slides = swiper.slides;
+        for (let i = 0; i < slides.length; i++) {
+          const slide = slides.eq(i);
+          slide.css('zIndex', 100 - i); //设置slide的z-index层级
+        }
+      }}
+      onResize={(swiper) => {
+        swiper.update();
+      }}
+      onSetTranslate={(swiper) => {
+        console.log('onSetTranslate')
+        // 手动设置wrapper的位移
+        const slides = swiper.slides;
+        let offsetAfter = swiper.width * 0.95; //每个slide的位移值
+        for (let i = 0; i < slides.length; i++) {
+          let slide = slides.eq(i);
+          let progress = slides[i].progress;
+          if (progress <= 0) {
+            //右边slide位移
+            slide.transform(
+              'translate3d(' +
+                progress * offsetAfter +
+                'px, 0, 0) scale(' +
+                (1 - Math.abs(progress) / 20) +
+                ')'
+            );
+            slide.css('opacity', progress + 3); //最右边slide透明
+          }
+          if (progress > 0) {
+            slide.transform('rotate(' + -progress * 5 + 'deg)'); //左边slide旋转
+            slide.css('opacity', 1 - progress); //左边slide透明
+          }
+        }
+      }}
+      onSetTransition={(swiper, transition) => {
+        console.log('onSetTransition')
+        for (let i = 0; i < swiper.slides.length; i++) {
+          // .eq(index)   返回当前选中的元素中的指定序号的元素
+          // https://www.swiper.com.cn/usage/dom7/index.html
+          let slide = swiper.slides.eq(i);
+          slide.transition(transition);
+        }
+      }}
     >
       {slidesArr}
     </Swiper>
